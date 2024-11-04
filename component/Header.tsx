31b2ba867d6b4aacc989794aa850e7bc5b66ab39
kahkashan25SH <ahmedalikahkashan@gmail.com>
2024-11-05 02:03:58 +0500

    first commit

---
+++ component/Header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+const Header = () => {
+  return (
+
+        <div className="bg-[#A29054] h-[88px] p-2 text-white font-serif text-[24px]">MANZZARI</div>
+    
+
+  )
+}
+
+export default Header
